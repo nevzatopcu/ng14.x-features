10844bbf83dd123b9316998e6f9e0789a16ba20f
nevzatopcu <nevzatopcu@gmail.com>
2022-10-12 20:50:26 +0300

    Standalone NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+    <button (click)="show = !show">{{ show ? 'hide' : 'show' }}</button>
+    show = {{ show }}
+    <br />
+    <div *ngIf="show">Text to show</div>
+  `,
   standalone: true,
+  imports: [NgIf],
 })
-export class AppComponent {}
+export class AppComponent {
+  show = true;
+}
